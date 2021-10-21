5a8b767a15fc378aa1a2e5661dcd22b8cf4f51db
Paavveel <pa.tomilin@gmail.com>
2021-10-21 15:45:01 +0300

    Применяет styled-componets к App и app-header

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,16 +5,24 @@ import PostStatusFilter from '../post-status-filter';
 import PostList from '../post-list';
 import PostAddForm from '../post-add-form';
 import './app.css';
+import styled from 'styled-components';
+
+const AppBlock = styled.div`
+  margin: 0 auto;
+  max-width: 800px;
+`;
 
 const App = () => {
   const data = [
+    5,
+    {},
     { label: 'Going to learn React', important: true, id: 'qwer' },
     { label: 'That is good', important: false, id: 'asdf' },
     { label: 'I need a break...', important: false, id: 'zxcv' },
   ];
 
   return (
-    <div className='app'>
+    <AppBlock>
       <AppHeader />
       <div className='search-panel d-flex'>
         <SearchPanel />
@@ -22,7 +30,7 @@ const App = () => {
       </div>
       <PostList posts={data} />
       <PostAddForm />
-    </div>
+    </AppBlock>
   );
 };
 
--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -3,15 +3,27 @@ import PostListItem from '../post-list-item';
 import './post-list.css';
 
 const PostList = ({ posts }) => {
-  const elements = posts.map((item) => {
-    const { id, ...itemsProps } = item;
+  const filteredPosts = posts.filter(
+    (item) => typeof item === 'object' && isNotEmpty(item)
+  );
+
+  const elements = filteredPosts.map((item) => {
+    const { id, ...itemProps } = item;
     return (
       <li key={id} className='list-group-item'>
-        <PostListItem {...itemsProps} />
+        <PostListItem {...itemProps} />
       </li>
     );
   });
 
+  function isNotEmpty(obj) {
+    for (let key in obj) {
+      return true;
+    }
+    return false;
+  }
+
   return <ul className='app-list list-group'>{elements}</ul>;
 };
+
 export default PostList;
